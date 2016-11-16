790e084d800a6a2eabd22f802cf6e9978efad5dd
KevinSia <calvin.kl1026@gmail.com>
2016-11-16 15:58:09 +0800

    complete profile part 1

---
+++ InstagramClone/InstagramClone/Base.lproj/Views/FriendPostCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FriendPostCollectionViewCell.swift
+//  InstagramClone
+//
+//  Created by Students on 11/15/16.
+//  Copyright © 2016 Panda^4. All rights reserved.
+//
+
+import UIKit
+
+class FriendPostCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var friendsPostImage: UIImageView!
+    
+    
+}
---
+++ InstagramClone/InstagramClone/Base.lproj/Views/UserPostCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserPostCollectionViewCell.swift
+//  InstagramClone
+//
+//  Created by Students on 11/16/16.
+//  Copyright © 2016 Panda^4. All rights reserved.
+//
+
+import UIKit
+
+class UserPostCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var userPostImage: UIImageView!
+    
+}
